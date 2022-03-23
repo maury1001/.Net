6524207cf5c1b2b5cd1278035bbd0c3b9369a7a8
Mauricio <mauricioallaman71@gmail.com>
2022-03-23 11:29:36 -0300

    comit nuevo

---
+++ Etapa1/Entidades/Escuela.cs
@@ -0,0 +1,37 @@
+using CoreEscuela.Entidades;
+
+namespace CoreEscuela.Escuela
+{
+    class Escuela
+    {
+        string nombre;
+
+        public string Nombre
+        {
+            get{return nombre;}
+            set{nombre = value;}
+        }
+
+        public int AñoDeCreacion{get;set;}
+
+        public string Pais{get;set;}
+        public string Ciudad { get; set; }
+        
+        public TiposEscuela TipoEscuela{get;set;}
+
+
+        //constructor tradicional
+        // public Escuela (string nombre, int año)
+        // {
+        //     this.nombre = nombre;
+        //     this.AñoDeCreacion = año;
+        // }
+        
+        public Escuela (string nombre, int año) => (Nombre, AñoDeCreacion) = (nombre, año);
+
+        public override string ToString()
+        {
+            return $"Nombre{Nombre}, Tipo{TipoEscuela} \n Pais{Pais}, Ciudad{Ciudad}";
+        }
+    }
+}
---
+++ Etapa1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CoreEscuela.Entidades;
+using CoreEscuela.Escuela;
+
+namespace Etapa1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var escuela = new Escuela("UNTDF",2013);
+            escuela.Pais = "Argentina";
+            escuela.Ciudad = "Ushuaia";
+            escuela.TipoEscuela = TiposEscuela.Primaria;
+            Console.WriteLine(escuela);
+
+        }
+    }
+
+}
