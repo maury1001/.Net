2f500c2f6f7a32ee13b6e1229dd52407d4cf6ac7
Mauricio <mauricioallaman71@gmail.com>
2022-04-05 10:48:07 -0300

    otro comit

--- Etapa1/Entidades/Evaluaciones.cs
+++ Etapa1/Entidades/Evaluaciones.cs
@@ -5,6 +5,10 @@ namespace CoreEscuela.Entidades
         public string UniqueId { get; private set; }
         public string Nombre { get; set; }
 
+        public Alumno Alumno { get; set; }
+        public Asignatura Asignatura { get; set; }
+        public float Nota { get; set; }
+
          public Evaluaciones()
         {
             UniqueId = Guid.NewGuid().ToString();
