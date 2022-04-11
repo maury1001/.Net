f8f048138cc69cc0debe21dfc90ae7d98ef7434a
Mauricio <mauricioallaman71@gmail.com>
2022-04-11 11:07:25 -0300

    comit nuevo

--- Etapa1/App/EscuelaEngine.cs
+++ Etapa1/App/EscuelaEngine.cs
@@ -71,6 +71,23 @@ namespace CoreEscuela
                 }
             }
         }
+         public List<ObjetoEscuelaBase> GetObjetosEscuela()
+            {
+                var listaObj = new List<ObjetoEscuelaBase>();
+                listaObj.Add(Escuela);
+                listaObj.AddRange(Escuela.Cursos);
+                foreach (var curso in Escuela.Cursos)
+                {
+                   listaObj.AddRange(curso.Asignaturas);
+                   listaObj.AddRange(curso.Alumnos);
+                   foreach (var alumno in curso.Alumnos)
+                   {
+                       listaObj.AddRange(alumno.Evaluaciones);
+                   }
+                }
+                
+                return listaObj;
+            }
 
 
 
